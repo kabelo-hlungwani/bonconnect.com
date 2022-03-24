974bc6e0b2c51ab172ada6bfecddefbe7868280c
Kabelo Mighty Hlungwani <53508373+kabelo-mighty@users.noreply.github.com>
2022-03-24 02:48:03 +0200

    Add files via upload

---
+++ error.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html style="background: #ffffff;">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Error 404</title>
+	<link rel="icon" href="assets/img/logo.png">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Almarai">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="assets/fonts/simple-line-icons.min.css">
+    <link rel="stylesheet" href="assets/fonts/typicons.min.css">
+    <link rel="stylesheet" href="assets/css/3-Cards-per-Row.css">
+    <link rel="stylesheet" href="assets/css/Footer-Basic.css">
+    <link rel="stylesheet" href="assets/css/Footer-Dark.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
+    <link rel="stylesheet" href="assets/css/Map-Clean.css">
+    <link rel="stylesheet" href="assets/css/Navigation-Clean.css">
+    <link rel="stylesheet" href="assets/css/untitled.css">
+</head>
+
+<body style="background: rgb(248, 249, 250);">
+    <section class="text-center bg-light features-icons" style="background: rgb(248, 249, 250);margin-top: 94px;">
+        <h1 style="font-family: Lato, sans-serif;text-align: center;font-size: 26px;font-weight: 400;color: #323e50;">Error 404</h1>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-1"></div>
+                <div class="col-lg-10" style="border-top-left-radius: 15px;background: #838ea0;border-top-right-radius: 15px;border-bottom-right-radius: 15px;border-bottom-left-radius: 15px;color: var(--white);">
+                    <div class="mx-auto features-icons-item mb-5 mb-lg-0 mb-lg-3">
+                        <div class="d-flex features-icons-icon"><i class="typcn typcn-radar-outline m-auto text-primary" data-bss-hover-animate="pulse" style="color: #c1bfbf;font-size: 50px;"></i></div>
+                        <h3 style="font-size: 16px;font-family: Almarai, sans-serif;font-weight: 400;">Page Coming soon</h3><a href="index.php" style="color: var(--white);font-family: Almarai, sans-serif;font-size: 14px;">Go to homepage</a>
+                    </div>
+                </div>
+                <div class="col-lg-1"></div>
+            </div>
+        </div>
+    </section>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/bs-init.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Bonconnect</title>
+	<link rel="icon" href="assets/img/logo.png">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Almarai">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="assets/fonts/simple-line-icons.min.css">
+    <link rel="stylesheet" href="assets/fonts/typicons.min.css">
+    <link rel="stylesheet" href="assets/css/3-Cards-per-Row.css">
+    <link rel="stylesheet" href="assets/css/Footer-Basic.css">
+    <link rel="stylesheet" href="assets/css/Footer-Dark.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
+    <link rel="stylesheet" href="assets/css/Map-Clean.css">
+    <link rel="stylesheet" href="assets/css/Navigation-Clean.css">
+    <link rel="stylesheet" href="assets/css/untitled.css">
+</head>
+
+<body style="background: rgb(248, 249, 250);">
+    <nav class="navbar navbar-light navbar-expand-md navigation-clean" style="background: rgb(248,249,250);">
+        <div class="container"><img src="assets/img/logo.png" style="height: 65px;"><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1" style="border-color: transparent;"><span class="sr-only">Toggle navigation</span><i class="typcn typcn-th-small" style="color: #323e50;"></i></button>
+            <div class="collapse navbar-collapse" id="navcol-1" style="font-size: 14px;font-family: Almarai, sans-serif;">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item"><a class="nav-link" href="#service">Services</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#about">About Us</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#contact">Contact Us</a></li>
+                    <li class="nav-item"><a class="nav-link" href="error.php">Vacancies</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <header class="text-center text-white masthead" style="background: url(&quot;assets/img/210888.jpg&quot;);background-size: cover;height: 381.223px;filter: hue-rotate(0deg) invert(0%);">
+        <div class="overlay"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-9 mx-auto" data-aos="fade" data-aos-duration="600" data-aos-delay="250" style="margin-top: -98px;">
+                    <h1 class="mb-5" style="font-size: 26px;font-family: Lato, sans-serif;font-weight: 400;">BACKGROUND</h1>
+                    <h1 class="mb-5" style="font-size: 16px;font-weight: 300;font-family: Almarai, sans-serif;text-align: center;"><br>Bon Connect is a 100% black-owned company that was established in 2015. It is a true&nbsp; African-biased model with the characteristics of sound business in Africa. As a result of&nbsp;which its ownership is based on a black economic empowerment initiative in which&nbsp;shareholders carry XYL capital, black management and staff confidence and civil&nbsp; organization.<br><br></h1>
+                </div>
+            </div>
+        </div>
+    </header>
+    <section id="service" class="bg-light">
+        <div class="container" style="margin-top: -74px;">
+            <div class="row">
+                <div class="col-sm-10 col-md-6 col-lg-4 offset-sm-1 offset-md-0 order-md-1 order-lg-1" style="margin-top: 20px;">
+                    <div class="border rounded shadow bg-white" data-aos="fade" data-aos-duration="600" data-aos-delay="200" data-aos-once="true">
+                        <div class="text-center w-100"><i class="typcn typcn-html5" style="font-size: 80px;color: #323e50;"></i></div>
+                        <div style="margin-top: 12px;padding: 12px;">
+                            <h5 class="text-center" style="font-family: Lato, sans-serif;color: #323e50;font-size: 14px;font-weight: 400;">Web Development</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-10 col-md-6 col-lg-4 offset-sm-1 offset-md-0 order-md-1 order-lg-1" style="margin-top: 20px;">
+                    <div class="border rounded shadow bg-white" data-aos="fade" data-aos-duration="600" data-aos-delay="200" data-aos-once="true">
+                        <div class="text-center w-100"><i class="typcn typcn-plug" style="font-size: 80px;color: #323e50;"></i></div>
+                        <div style="margin-top: 12px;padding: 12px;">
+                            <h5 class="text-center" style="font-family: Lato, sans-serif;color: #323e50;font-size: 14px;font-weight: 400;">5G Networking</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-10 col-md-6 col-lg-4 offset-sm-1 offset-md-0 order-md-1 order-lg-1" style="margin-top: 20px;">
+                    <div class="border rounded shadow bg-white" data-aos="fade" data-aos-duration="600" data-aos-delay="200" data-aos-once="true">
+                        <div class="text-center w-100"><i class="typcn typcn-device-phone" style="font-size: 80px;color: #323e50;"></i></div>
+                        <div style="margin-top: 12px;padding: 12px;">
+                            <h5 class="text-center" style="font-family: Lato, sans-serif;color: #323e50;font-size: 14px;font-weight: 400;">Mobile App Development</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-10 col-md-6 col-lg-4 offset-sm-1 offset-md-0 order-md-1 order-lg-1" style="margin-top: 20px;">
+                    <div class="border rounded shadow bg-white" data-aos="fade" data-aos-duration="600" data-aos-delay="200" data-aos-once="true">
+                        <div class="text-center w-100"><i class="typcn typcn-chart-line-outline" style="font-size: 80px;color: #323e50;"></i></div>
+                        <div style="margin-top: 12px;padding: 12px;">
+                            <h5 class="text-center" style="font-family: Lato, sans-serif;color: #323e50;font-size: 14px;font-weight: 400;">Internet Service Provider</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-10 col-md-6 col-lg-4 offset-sm-1 offset-md-0 order-md-1 order-lg-1" style="margin-top: 20px;">
+                    <div class="border rounded shadow bg-white" data-aos="fade" data-aos-duration="600" data-aos-delay="200" data-aos-once="true">
+                        <div class="text-center w-100"><i class="typcn typcn-device-desktop" style="font-size: 80px;color: #323e50;"></i></div>
+                        <div style="margin-top: 12px;padding: 12px;">
+                            <h5 class="text-center" style="font-family: Lato, sans-serif;color: #323e50;font-size: 14px;font-weight: 400;">Internet Of Things</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-10 col-md-6 col-lg-4 offset-sm-1 offset-md-0 order-md-1 order-lg-1" style="margin-top: 20px;">
+                    <div class="border rounded shadow bg-white" data-aos="fade" data-aos-duration="600" data-aos-delay="200" data-aos-once="true">
+                        <div class="text-center w-100"><i class="typcn typcn-chart-pie-outline" style="font-size: 80px;color: #323e50;"></i></div>
+                        <div style="margin-top: 12px;padding: 12px;color: #323e50;">
+                            <h5 class="text-center" style="font-family: Lato, sans-serif;color: #323e50;font-size: 14px;font-weight: 400;">Fintech Solutions</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="text-center bg-light features-icons" style="background: rgb(248, 249, 250);margin-top: -67px;">
+        <h1 id="about" style="font-family: Lato, sans-serif;text-align: center;font-size: 26px;font-weight: 400;color: #323e50;">WHO ARE WE?</h1>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="mx-auto features-icons-item mb-5 mb-lg-0 mb-lg-3">
+                        <div class="d-flex features-icons-icon"><i class="icon-eye m-auto text-primary" data-bss-hover-animate="pulse" style="color: #074980;font-size: 50px;"></i></div>
+                        <h3 style="font-size: 16px;">Vision</h3>
+                        <p class="lead mb-0" style="font-family: Almarai, sans-serif;font-size: 14px;">To be a world pioneer in ICT outsourcing and global base solutions on an international scale.<br></p>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="mx-auto features-icons-item mb-5 mb-lg-0 mb-lg-3">
+                        <div class="d-flex features-icons-icon"><i class="icon-trophy m-auto text-primary" data-bss-hover-animate="pulse" style="color: #074980;font-size: 50px;"></i></div>
+                        <h3 style="font-size: 16px;font-family: Lato, sans-serif;">Mission</h3>
+                        <p class="lead mb-0" style="font-family: Almarai, sans-serif;font-size: 14px;">To be an organization with a comprehensive value proposition that offers world quality and standards in the area of information technology. To serve as a focal point for a variety of excellent facilities.<br></p>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="mx-auto features-icons-item mb-5 mb-lg-0 mb-lg-3">
+                        <div class="d-flex features-icons-icon"><i class="icon-bulb m-auto text-primary" data-bss-hover-animate="pulse" style="color: #323e50;font-size: 50px;"></i></div>
+                        <h3 style="font-size: 16px;">Easy to Use</h3>
+                        <p class="lead mb-0" style="font-family: Almarai, sans-serif;font-size: 14px;">We are focused on results and success. Stakeholder satisfaction is something we aim for. We adopt the principles of cooperative governance. Our products and services meet To be a world pioneer in ICT outsourcing and global base solutions on an international scale.<br></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="jumbotron" data-aos="fade" data-aos-duration="600" data-aos-delay="250" style="background: rgb(248, 249, 250);margin-top: -120px;">
+            <h1 style="font-family: Lato, sans-serif;text-align: center;font-size: 26px;font-weight: 400;color: #323e50;">ABOUT US</h1>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 col-lg-1"></div>
+                    <div class="col-md-4 col-lg-10">
+                        <p style="font-family: Almarai, sans-serif;font-size: 14px;text-align: justify;"><br>Bon Connect is committed to the employment equity concept enshrined in the Employment Equity Act, however, the international market requires Bon Connect to prepare ahead of time through its’ management succession strategy and policies. For its global operations policy, Bon Connect is working on diversity management and gender sensitivity and it has invested in an orientation program for new hires in the executive management program for company development and continuity. Bon Connect is decided to empowering the majority of people in the countries where it works, for example, In South Africa, the majority of senior management is made up of black people, including black women. The cooperation has a 65% female employment policy and an 80% youth employment policy.<br><br></p>
+                    </div>
+                    <div class="col-md-4 col-lg-1"></div>
+                </div>
+            </div>
+            <p style="text-align: center;"><a class="btn btn-sm" role="button" style="font-family: Almarai, sans-serif;border-top-left-radius: 0px;border-top-right-radius: 5px;border-bottom-right-radius: 0px;border-bottom-left-radius: 5px;background: #323e50;color: rgb(255,255,255);" href="assets/profile/profile.pdf"><i class="icon ion-ios-cloud-download-outline"></i>&nbsp; Profile</a></p>
+        </div>
+    </section>
+    <div class="container" data-aos="fade" data-aos-duration="600" data-aos-delay="250" style="margin-top: -62px;">
+        <h1 id="contact" style="text-align: center;font-size: 26px;font-weight: 400;font-family: Lato, sans-serif;color: #323e50;">CONTACT US</h1>
+        <div class="row" style="margin-bottom: 36px;">
+            <div class="col-md-8">
+                <section class="map-clean">
+                    <div class="container">
+                        <div class="intro"></div>
+                    </div><iframe allowfullscreen="" frameborder="0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCEe3-wdRHXj_9rLAyN2SyQr6lkbWuH8i4&amp;q=88+Marshall+Street+%2C2nd+Floor+Samancor+House%2CMarshalltown%2CJohannesburg+%2C2107&amp;zoom=15" width="100%" height="450" style="margin-top: -19px;"></iframe>
+                </section>
+            </div>
+            <div class="col-md-4">
+                <h3 class="my-3" style="font-size: 16px;font-weight: 400;"><i class="typcn typcn-location" style="color: #323e50;"></i>Business Address</h3>
+                <p style="font-family: Almarai, sans-serif;font-size: 14px;">88 Marshall Street&nbsp;<br>2nd Floor Samancor House<br>Marshalltown<br>Johannesburg&nbsp;<br>2107<br></p>
+                <h3 class="my-3" style="font-family: Lato, sans-serif;font-weight: 400;font-size: 16px;"><i class="typcn typcn-time" style="color: #323e50;"></i>Working Hours</h3>
+                <ul class="list-unstyled">
+                    <li style="font-size: 14px;font-family: Almarai, sans-serif;">08H00 - 17H00</li>
+                </ul>
+                <h3 class="my-3" style="font-family: Lato, sans-serif;font-weight: 400;font-size: 16px;"><i class="typcn typcn-phone-outline" style="color: #323e50;"></i>Contact Details</h3>
+                <ul class="list-unstyled">
+                    <li style="font-size: 14px;">&nbsp;<i class="typcn typcn-device-phone" style="font-size: 16px;color: #323e50;"></i>&nbsp;068 159 6956<br></li>
+                    <li style="font-size: 14px;">&nbsp;<i class="typcn typcn-mail" style="font-size: 18px;color: #323e50;"></i>&nbsp;Info@bonconnect.com<br></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <footer class="footer-basic" style="background: #323e50;">
+        <div class="social" style="font-size: 28px;"><a href="#" style="font-size: 16px;border-color: #323e50;"><i class="icon ion-social-instagram" style="color: rgb(255,255,255);"></i></a><a href="#" style="font-size: 16px;border-color: #323e50;"><i class="icon ion-social-snapchat" style="color: rgb(255,255,255);"></i></a><a href="#" style="font-size: 16px;border-color: #323e50;"><i class="icon ion-social-twitter" style="color: rgb(255,255,255);"></i></a><a href="#" style="font-size: 16px;border-color: #323e50;"><i class="icon ion-social-facebook" style="color: rgb(255,255,255);"></i></a></div>
+        <ul class="list-inline" style="font-size: 14px;font-family: Lato, sans-serif;color: rgb(255,255,255);">
+            
+            <li class="list-inline-item"><a href="#service">Services</a></li>
+            <li class="list-inline-item"><a href="#about">About</a></li>
+            <li class="list-inline-item"><a href="#contact">Contact</a></li>
+            <li class="list-inline-item" style="font-family: Almarai, sans-serif;"><a href="error.php">Vacancies</a></li>
+        </ul>
+        <p class="copyright" style="font-size: 14px;color: rgb(255,255,255);font-family: Almarai, sans-serif;">Bonconnect © 2015-<?php echo date("Y"); ?></p>
+    </footer>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/bs-init.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
+</body>
+
+</html>
